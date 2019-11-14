2188bf11061d400485de3044274c354f7996d380
KADIM Ahmed <akadim70@gmail.com>
2019-11-14 18:07:54 +0100

    Adding a Routing parameter

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,8 @@ import { UserEditComponent } from './users/user-edit/user-edit.component';
 const routes: Routes = [
     { path: 'users', component: UsersComponent },
     { path: 'user/:id', component: UserEditComponent },
-    { path: 'user', component: UserEditComponent }
+    { path: 'user', component: UserEditComponent },
+    { path: '', redirectTo: '/users', pathMatch: 'full' }
 ];
 
 @NgModule({
